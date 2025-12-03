4917dedf06cb3106f699d2ab8d9732052455ef7b
Kevin Neyer <kevinneyer@gmail.com>
2025-12-02 23:19:46 -0500

    start show beer container

--- src/containers/BeerPageContainer.tsx
+++ src/containers/BeerPageContainer.tsx
@@ -3,18 +3,19 @@ import { useEffect, useState } from "react";
 
 const BeerPageContainer = () => {
     const { beerId } = useParams();
+    const [showBeer, setShowBeer] = useState<BeerInterface | null>(null)
 
     useEffect(() => {
         fetch(`http://127.0.0.1:3000/api/v1/beers/${beerId}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            setShowBeer(data);
         })
-    }, [])
+    });
 
     return (
         <>
-            Show Page
+            {showBeer ? showBeer.name : ''}
         </>
     )
 };
